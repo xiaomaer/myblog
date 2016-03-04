f326a6e4f1d9834427abb164a0b96691e66ecc96
xiaoma <523441916@qq.com>
2016-03-04 15:06:49 +0800

    添加“使position:fixed样式水平滚动的方法”

--- js/xiaoma.js
+++ js/xiaoma.js
@@ -1,13 +1,8 @@
 var doc = document,
     topbtn = doc.getElementById("totop");
-//窗口滚动事件，当滚动时，显示“回到顶部”按钮
-doc.addEventListener("scroll", showTopbtn, false);
-//点击“回到顶部”按钮，回到页面顶部
-topbtn.addEventListener("click", toTop, false);
 //显示“回到顶部”按钮
-function showTopbtn(event) {
-    var e = event || window.event,
-        scrollTop = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+function showTopbtn() {
+    var scrollTop = window.pageYOffset || doc.body.scrollTop || doc.documentElement.scrollTop;
     //显示回到顶部图标
     if (scrollTop >= 100 && topbtn.style.display !== "block") {
         topbtn.style.display = "block";
@@ -16,6 +11,7 @@ function showTopbtn(event) {
     if (scrollTop <= 10 && topbtn.style.display === "block") {
         topbtn.style.display = "none";
     }
+    scrollFixed();
 }
 //回到页面顶部，“回到顶部”按钮消失
 function toTop() {
@@ -30,11 +26,24 @@ function toTop() {
             clearInterval(timer);
             topbtn.style.display = "none";
         }
-
     }, 1);
 }
+//使position:fixed样式水平滚动的方法
+//原理就是当window对象发生scroll事件和resize事件时，更新div块的left或right的值，使其相对浏览器左边或右边的位置实时改变。
+function scrollFixed(){
+   var scrollLeft=window.pageXOffset||doc.body.scrollLeft||doc.documentElement.scrollLeft;
+    if(scrollLeft>0){
+        doc.getElementById("header").style.left=-scrollLeft+"px";
+    }
+}
+//窗口滚动事件，当滚动时，显示“回到顶部”按钮
+doc.addEventListener("scroll", showTopbtn, false);
+//点击“回到顶部”按钮，回到页面顶部
+topbtn.addEventListener("click", toTop, false);
 //搜索按钮
 doc.getElementById("search").addEventListener("submit", queryArticle, false);
+//页面大小改变时
+window.addEventListener("resize",scrollFixed,false);
 //提交表单，根据关键字搜索符合条件的文档列表
 function queryArticle() {
     var keyword = doc.getElementById("query").value,
